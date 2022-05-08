f5b6cad6c23d2663c5334069383ef174a2f680ad
Arjun Singh <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-05-09 00:01:15 +0530

    Add files via upload

---
+++ day6/product.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/product-of-array-except-self/submissions/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var productExceptSelf = function(nums) {
+  var output = [];
+var leftMult = 1;
+var rightMult = 1;
+for (var i=nums.length - 1; i >= 0; i--) {
+    output[i] = rightMult;
+    rightMult *= nums[i];
+}
+for (var j=0; j < nums.length; j++) {
+    output[j] *= leftMult;
+    leftMult *= nums[j];
+}
+return output;
+};
